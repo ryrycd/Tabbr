634ad98a323ee5ccf30edc822eb2046296671389
ryrycd <31199861+ryrycd@users.noreply.github.com>
2022-10-16 01:23:40 +0900

    Update Quote.js

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -37,7 +37,7 @@ const greeting = "Test greeting using variable";
 };
     setQuoteLoading(false);
     console.log(data);
-    console.log(`${greeting} Marcus`)
+    console.log(greeting);
     return data;
   };
 
