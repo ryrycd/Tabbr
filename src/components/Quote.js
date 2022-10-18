48507c45ae8addec70a0b04e6c8a6c8410e34ab7
ryrycd <31199861+ryrycd@users.noreply.github.com>
2022-10-19 01:07:34 +0900

    Update Quote.js

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -18,6 +18,7 @@ const months = ["January", "February", "March", "April", "May", "June", "July"];
 
 const random = Math.floor(Math.random() * months.length);
 console.log(random, months[random]);
+    var RandomQuote = random;
     
     const data = {
     "id": 0,
