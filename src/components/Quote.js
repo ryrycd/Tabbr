fe1003a69dc2eef307d161e9f8f9171f8ed528f9
ryrycd <31199861+ryrycd@users.noreply.github.com>
2022-10-19 10:18:30 +0900

    Update Quote.js

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -21,103 +21,14 @@ var random = Math.floor(Math.random() * months.length);
     let QuoteSource = "";
     let QuoteName = "Unknown";
 
-    if (RandomQuote === "Over 2 billion people’s livelihoods depend on forests.") {
+    if (RandomQuote === "Over 2 billion people’s livelihoods depend on forests." ^ "Forests are home to 300 million people around the world." ^ "Trees planted sequester CO2 at an average of 25 kilos per tree per year; or an average of 250 kilos over a tree’s lifetime." ^ "There are more than 60,000 species of trees in the world." ^ "More than a quarter of modern medicines originate from tropical forest plants." ^ "Forests are home to 80% of the world's terrestrial biodiversity." ^ "Tropical deforestation contributes 20% of annual global green-house pas emissions." ^ "13 million forests provide jobs to more than 13 million people across the world." ^ "80% of Europe used to be forested land, but only 34% of it remains today." ^ "40% of the world's oxygen is produced from rainforest." ^ "Forests support over 86 million green jobs and the livelihoods of millions of other. More than 90% of those who live in extreme poverty are forest-dependent." ^ "25% of our carbon emissions have historically been captured by Earth's forests, farms, and grasslands to keep the landscapes vegetated and soil hydrated.") {
 QuoteSource = "https://www.ecomatcher.com/tree-resources/";
       QuoteName = "ecomatcher.com";
+} else {
+  QuoteSource = "https://www.precisiontreemn.com/tips/14-fun-facts-about-trees.html";
+      QuoteName = "Precision Trees";
 }
-    
-       if (RandomQuote === "Forests are home to 300 million people around the world.") {
-QuoteSource = "https://www.ecomatcher.com/tree-resources/";
-         QuoteName = "ecomatcher.com";
-}
-    if (RandomQuote === "Trees planted sequester CO2 at an average of 25 kilos per tree per year; or an average of 250 kilos over a tree’s lifetime.") {
-QuoteSource = "https://www.ecomatcher.com/tree-resources/";
-      QuoteName = "ecomatcher.com";
-}
-    
-    if (RandomQuote === "There are more than 60,000 species of trees in the world.") {
-QuoteSource = "https://www.ecomatcher.com/tree-resources/";
-      QuoteName = "ecomatcher.com";
-}
-    
-   if (RandomQuote === "More than a quarter of modern medicines originate from tropical forest plants.") {
-QuoteSource = "https://www.ecomatcher.com/tree-resources/";
-     QuoteName = "ecomatcher.com";
-}
-       if (RandomQuote === "Forests are home to 80% of the world’s terrestrial biodiversity.") {
-QuoteSource = "https://www.ecomatcher.com/tree-resources/";
-         QuoteName = "ecomatcher.com";
-}
-       if (RandomQuote === "Tropical deforestation contributes 20% of annual global green-house pas emissions.") {
-QuoteSource = "https://www.ecomatcher.com/tree-resources/";
-         QuoteName = "ecomatcher.com";
-}
-       if (RandomQuote === "13 million forests provide jobs to more than 13 million people across the world.") {
-QuoteSource = "https://www.ecomatcher.com/tree-resources/";
-         QuoteName = "ecomatcher.com";
-}
-       if (RandomQuote === "80% of Europe used to be forested land, but only 34% of it remains today.") {
-QuoteSource = "https://www.ecomatcher.com/tree-resources/";
-         QuoteName = "ecomatcher.com";
-}
-       if (RandomQuote === "40% of the world’s oxygen is produced from rainforest.") {
-QuoteSource = "https://www.ecomatcher.com/tree-resources/";
-         QuoteName = "ecomatcher.com";
-}
-       if (RandomQuote === "Forests support over 86 million green jobs and the livelihoods of millions of other. More than 90% of those who live in extreme poverty are forest-dependent.") {
-QuoteSource = "https://www.ecomatcher.com/tree-resources/";
-         QuoteName = "ecomatcher.com";
-}
-       if (RandomQuote === "25% of our carbon emissions have historically been captured by Earth’s forests, farms, and grasslands to keep the landscapes vegetates and soil hydrated.") {
-QuoteSource = "https://www.ecomatcher.com/tree-resources/";
-         QuoteName = "ecomatcher.com";
-}
-       if (RandomQuote === "Trees are the longest living organisms on Earth, and never die of old age.") {
-QuoteSource = "https://www.precisiontreemn.com/tips/14-fun-facts-about-trees.html";
-         QuoteName = "Precision Tree";
-}
-      if (RandomQuote === "Tree rings can help predict climate change.") {
-QuoteSource = "https://www.precisiontreemn.com/tips/14-fun-facts-about-trees.html";
-        QuoteName = "Precision Tree";
-}
-    if (RandomQuote === "Strategically planting trees and shrubs to maximize shade can save you up to 25% on your electricity bills.") {
-QuoteSource = "https://www.precisiontreemn.com/tips/14-fun-facts-about-trees.html";
-      QuoteName = "Precision Tree";
-}
-    if (RandomQuote === "NASA took tree seeds to the moon during the Apollo 14 mission in 1971, testing to see if space’s orbit could change tree growth.") {
-QuoteSource = "https://www.precisiontreemn.com/tips/14-fun-facts-about-trees.html";
-      QuoteName = "Precision Tree";
-}
-    if (RandomQuote === "Trees are able to communicate with each other, and can signal danger to other trees when being attacked so they can start their own defense.") {
-QuoteSource = "https://www.precisiontreemn.com/tips/14-fun-facts-about-trees.html";
-      QuoteName = "Precision Tree";
-}
-    if (RandomQuote === "Trees can defend themselves against attacking insect by releasing certain chemicals that make their leaves harder to digest.") {
-QuoteSource = "https://www.precisiontreemn.com/tips/14-fun-facts-about-trees.html";
-      QuoteName = "Precision Tree";
-}
-    if (RandomQuote === "Leaves, twigs, and branches on trees absorb and deflect sound waves to mask unwanted noise, through a process called sound attenuation.") {
-QuoteSource = "https://www.precisiontreemn.com/tips/14-fun-facts-about-trees.html";
-      QuoteName = "Precision Tree";
-}
-    if (RandomQuote === "Forests help lower our stress, as trees release chemicals called phytoncides. When we breathe them in, it can reduce blood pressure, lower anxiety levels, and increase our pain threshold.") {
-QuoteSource = "https://www.precisiontreemn.com/tips/14-fun-facts-about-trees.html";
-      QuoteName = "Precision Tree";
-}
-    if (RandomQuote === "Tree rings are natural compasses - if you’re in the northern hemisphere, you can see the rings of the tree grow slightly thicker on the southern side since it receives more light.") {
-QuoteSource = "https://www.precisiontreemn.com/tips/14-fun-facts-about-trees.html";
-      QuoteName = "Precision Tree";
-}
-    if (RandomQuote === "Several recent nationwide surveys show that mature trees in a well-landscaped yard can increase the value of a house by seven to 19 percent.") {
-QuoteSource = "https://www.precisiontreemn.com/tips/14-fun-facts-about-trees.html";
-      QuoteName = "Precision Tree";
-}
-    if (RandomQuote === "Trees prevent soil from eroding into our waterways, reduce stormwater runoff, and lessen flood damage.") {
-QuoteSource = "https://www.precisiontreemn.com/tips/14-fun-facts-about-trees.html";
-      QuoteName = "Precision Tree";
-}
-    
-    
+   
     
     const data = {
     "id": 0,
