2f61cb83e65df0e57cc344572fb5a0a2a86cef38
ryrycd <31199861+ryrycd@users.noreply.github.com>
2022-11-13 16:31:27 +0900

    Update Weather.js

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -6,28 +6,14 @@ const Weather = () => {
   const [errorMsg, setErrorMsg] = useState("");
   const [weatherData, setWeatherData] = useState([]);
   const [weatherIcon, setWeatherIcon] = useState("");
-
-var latitude;
-var longitude;
-var API = 'https://api.ipgeolocation.io/ipgeo?apiKey=665888c05b154f19982134897ae167ba';
-
-  fetch(API)           //api for the get request
-  .then(response => response.json())
-  // .then(data => {
-  //   latitude = data.latitude;
-  //  })
-  //  .then(data => {
-  //   longitude = data.longitude;
-  //  })
-  .then(data => console.log(data));
-  
-
-
   //gets the current location from geolocation and uses the lat and lon values as parameters for obtaining weather data
   const getWeather = async () => {
     if (2/2 === 1) {
 
-      
+
+     
+        let latitude = "35.6762";
+        let longitude = "139.6503";
 
         const response = await fetch(
           `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${process.env.REACT_APP_OPENWEATHER_KEY}`
