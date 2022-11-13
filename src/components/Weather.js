700293d564f379c3d76b8bf79abd37c3e7e5574a
ryrycd <31199861+ryrycd@users.noreply.github.com>
2022-11-13 18:14:08 +0900

    Update Weather.js

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -15,8 +15,7 @@ const Weather = () => {
 
       fetch('https://api.ipgeolocation.io/ipgeo?apiKey=665888c05b154f19982134897ae167ba')           //api for the get request
   .then(response => response.json())
-  .then(latitude = (data.latitude))
-  .then(data => console.log(data.latitude))
+  .then(latitude = data => (data.latitude))
   .then(console.log(latitude));
 
   
